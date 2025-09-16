379929467b426394a9a2c9c9461dbe99546c41b8
MinhTri-NTL <minhtri-ntl@MinhTri-NTLs-iMac.local>
2025-09-16 09:01:17 +0700

    Ignore android/app/.cxx

--- android/app/.cxx/Debug/6x3j275e/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/minhtri-ntl/.gradle/caches/8.14.3/transforms/e1242c86f794a0126cc864aec2525ab1/transformed/jetified-hermes-android-0.81.1-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/minhtri-ntl/.gradle/caches/8.14.3/transforms/e1242c86f794a0126cc864aec2525ab1/transformed/jetified-hermes-android-0.81.1-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
